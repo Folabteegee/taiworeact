88bf65e8871b49d069ffc7eaa2557b67b4e3cdf2
Taiwo <taiwoglory136@gmail.com>
2023-11-30 21:36:52 +0100

    did inputs

--- src/components/End.js
+++ src/components/End.js
@@ -4,7 +4,34 @@ import Message from "../icons/Message";
 export default function End() {
   return (
     <div className="end">
-      <div className="name-wrapper">
+      <div>
+        <div className="message1">
+          <Message />
+        </div>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="name-wrapper"
+          placeholder="Enter Name"
+          required
+        />
+      </div>
+
+      <div>
+        <div className="message2">
+          <Message />
+        </div>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          class="email-wrapper"
+          placeholder="Enter Email Address"
+          required
+        />
+      </div>
+      {/* <div className="name-wrapper">
         <div className="message">
           <Message />
         </div>
@@ -16,10 +43,8 @@ export default function End() {
           <Message />
         </div>
         <div className="name">Enter Email Address</div>
-      </div>
-
+      </div> */}
       <div className="contact">Contact Me</div>
-
       <div className="end-note">
         Hello Name, Your message has been sent to your email: Email address
       </div>
